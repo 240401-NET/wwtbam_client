3f36aaf6c67853135915fc922c4d5b244f32b422
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-05-04 09:53:35 -0400

    resolving merge conflict

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,31 @@
 import { defineConfig } from 'vite' 
+
 import react from '@vitejs/plugin-react-swc' 
+
 import mkcert from 'vite-plugin-mkcert' 
 
+ 
+
+// https://vitejs.dev/config/ 
+
 export default defineConfig({ 
+
     server: { 
+
         port: 5173, 
+
         proxy: { 
+
           "^/api": "https://wwtbam.azurewebsites.net" 
+
         } 
+
     }, 
   base: "/wwtbam_client/",
   plugins: [react(), mkcert()], 
+  
       build: {
+  
           outDir: 'dist', 
       }
 }) 
