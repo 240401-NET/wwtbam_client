4ab03ee875f2cc82c67fb27465d0fa478c0f5b56
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2024-05-08 14:16:31 -0500

    implement profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,15 +1,20 @@
 import logo from '../assets/logo.png';
 import formbg from '../assets/formbg.jpg';
 import Navbar from '../components/Navbar';
+import { useAuth } from "../context/AuthContext";
 
 const ProfilePage = () => {
+    const { user } = useAuth();
     return (
         <div className="bg-cover bg-center bg-no-repeat h-[100vh] w-full fixed bg-gradient-to-b from-black via-sky-950 to-black">
             <Navbar />
             <div className='flex flex-col items-center justify-center font-serif'>
-                <img src={logo} alt="logo" className="w-40 h-40 rounded-full mb-12" />
                 <div className="bg-cover bg-center bg-no-repeat h-full w-96 rounded-lg" style={{ backgroundImage: `url(${formbg})` }}>
                     <h1 className="text-yellow-600 font-bold text-3xl py-6 text-center font-serif">User Profile</h1>
+                    <h2 className='text-2xl font-bold text-center m-2 p-2'>User Name:</h2>
+                    <h3 className='text-xl text-center m-2 p-2'>{user?.userName}</h3>
+                    <h2 className='text-2xl font-bold text-center m-2 p-2'>E-mail</h2>
+                    <h3 className='text-xl text-center m-2 p-2'>{user?.email}</h3>
                 </div>
             </div>
         </div>
